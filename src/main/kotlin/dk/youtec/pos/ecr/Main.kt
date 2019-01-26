970fb01d970b27ba1ba699d804324043e1ab8346
Claus Holst <clh@visiolink.com>
2019-01-26 19:40:20 +0100

    Default 30 seconds delay until cancel is send

--- src/main/kotlin/dk/youtec/pos/ecr/Main.kt
+++ src/main/kotlin/dk/youtec/pos/ecr/Main.kt
@@ -9,7 +9,7 @@ suspend fun main(args: Array<String>) = coroutineScope {
     println("Point Finland ECR controller!")
 
     val amount = args.firstOrNull()?.toIntOrNull() ?: 0
-    val delaySeconds = args.getOrNull(1)?.toInt() ?: 20
+    val delaySeconds = args.getOrNull(1)?.toInt() ?: 30
 
     val controller = PointFinlandEcrController()
     controller.initiatePurchase(amount)
